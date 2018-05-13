c9ee7235e54b1e4c9d80fa8e07d581fe1e7f4506
Adela Giurgiu <adelaelenagiurgiu@gmail.com>
2018-05-13 18:45:52 +0300

    Remove populate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./models/Section");
-require("./populate");
+
 const AppRouter = require("./router");
 const router = AppRouter.Router;
 
