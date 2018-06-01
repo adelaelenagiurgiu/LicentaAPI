6d5071b625e2da54b06ae2584d9ba775c360d226
alexsirbu04 <alex.sirbu@tmnet.ro>
2018-06-02 02:46:24 +0300

    Changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const keys = require("./config/keys");
 require("./populate");
+
 const router = require("./router");
 
 mongoose.connect(keys.mongoURI);
